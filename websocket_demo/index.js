d4fef5c17f6712fae96e2d5b05fe51e52397dfe5
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-01-28 21:36:38 +0545

    connection between server and client using websocket full duplex!!!

--- websocket_demo/index.js
+++ websocket_demo/index.js
@@ -4,21 +4,28 @@ let connection=null;
 
 const httpserver=http.createServer((req,res)=>{
     console.log("request received");
-});
+})
 
 const websocket=new WebScoketServer({
     "httpServer":httpserver
 });
 
-websocket.on("request",(request)=>{
+websocket.on("request",request=>{
     connection=request.accept(null,request.origin);
-    connection.on("onopen",()=>console.log("connection opened"));
-    connection.on("onclose",()=>console.log("connection closed"));
-    connection.on("message",(message)=>{
-        console.log(`Received message ${message}`);
+    connection.on("open",()=>console.log("connection opened"));
+    connection.on("close",()=>console.log("connection closed"));
+    connection.on("message",message=>{
+        console.log(`Received message ${message.utf8Data}`);
     });
+    sendMessage();
 })
 
-httpserver.listen(3000,()=>{
-    console.log("server is listening on port 3000");
+httpserver.listen(8080,()=>{
+    console.log("server is listening on port 8080");
 });
+
+function sendMessage(){
+    connection.send(`Message ${Math.random()}`);
+
+    setTimeout(sendMessage,5000);
+}
